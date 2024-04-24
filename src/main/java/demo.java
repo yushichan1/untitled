2323653830430ed9342e636e52039d1b5acea5bc
ysc <1684431971@qq.com>
2024-04-24 17:06:47 +0800

    push test

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -3,5 +3,6 @@ public class demo {
         System.out.println("hello git");
         System.out.println("hot-fix test");
         System.out.println("hot-fix commit fix");
+        System.out.println("push test");
     }
 }
